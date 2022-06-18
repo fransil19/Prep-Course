34e6385f82ab7f02a244bed16f68459c9b952c37
Franco <xeneise19@gmail.com>
2022-06-17 21:01:54 -0300

    Finalizado prep 07-JS-6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,23 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let lista = nombre.split("")
+  lista[0] = lista[0].toUpperCase()
+  let cadena = lista.join("")
+  return cadena
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +28,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = 0
+  numeros.forEach(function(numero){
+    suma += numero
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento){
+    cb(elemento)
+  })
 }
 
 function map(array, cb) {
@@ -35,12 +49,22 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let nuevo_array = array.map(function(elemento){
+    return cb(elemento)
+  })
+  return nuevo_array
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const nuevo_array = array.filter(function(elemento){
+    let lista = elemento.split("")
+    return lista[0]==='a'
+    //return elemento.startsWith("a")
+  })
+  return nuevo_array
 }
 
 // No modificar nada debajo de esta línea
